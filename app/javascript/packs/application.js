eb3f8f32134e071f9ef9322ca9f45f4d65996b88
nari2239 <oryolife@gmail.com>
2021-01-03 15:58:06 +0900

    jQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 //= require swiper/js/swiper.js
 //= require swiper.js
 
